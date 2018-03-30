02d9165c06eb497e2643169b65db64a66725eab9
Maggie Summers <summers.maggie@gmail.com>
2018-03-30 11:02:08 -0700

    work on blog post preview component

--- src/app/blog-post-preview/blog-post-preview.component.ts
+++ src/app/blog-post-preview/blog-post-preview.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { blogPost } from '../blog-post.model';
+
 
 @Component({
   selector: 'app-blog-post-preview',
   templateUrl: './blog-post-preview.component.html',
   styleUrls: ['./blog-post-preview.component.css']
 })
+
 export class BlogPostPreviewComponent implements OnInit {
+  @Input() childBlogPostList: blogPost[];
+
+  console.log(blogPost);
 
   constructor() { }
 
